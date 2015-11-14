c95b72a2b0b8c4c3108938d323ad6d7c63fac723
saurabhj80 <saurabhj80@gmail.com>
2015-11-14 01:19:46 -0800

    Book class

--- BookSC/app/src/main/java/logic/Book.java
+++ BookSC/app/src/main/java/logic/Book.java
@@ -4,4 +4,15 @@ package logic;
  * Created by saurabhj80 on 10/21/15.
  */
 public class Book {
+
+    // Name
+    public final String name;
+
+    // Author
+    public final String author;
+
+    public Book(String name, String author) {
+        this.name =  name;
+        this.author = author;
+    }
 }
