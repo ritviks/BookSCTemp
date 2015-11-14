5e94a37587c9f932a243401e0e8d9d53e79feab3
saurabhj80 <saurabhj80@gmail.com>
2015-11-14 02:02:50 -0800

    Working

--- BookSC/app/src/main/java/Model/Book.java
+++ BookSC/app/src/main/java/Model/Book.java
@@ -1,18 +1,136 @@
 package Model;
 
+import java.security.acl.Owner;
+import java.sql.Blob;
+import java.util.Vector;
+
 /**
  * Created by saurabhj80 on 10/21/15.
  */
 public class Book {
 
     // Name
-    public final String name;
+    private String name;
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
     // Author
-    public final String author;
+    private String author;
 
-    public Book(String name, String author) {
-        this.name =  name;
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
         this.author = author;
     }
+
+
+    // ISBN
+    private long iSBN;
+
+    public long getiSBN() {
+        return iSBN;
+    }
+
+    public void setiSBN(long iSBN) {
+        this.iSBN = iSBN;
+    }
+
+    // Course
+    private String course;
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    // Picture
+    private Blob picture;
+    public Blob getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Blob picture) {
+        this.picture = picture;
+    }
+
+    // Quality
+    private int quality;
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    // Price
+    private int price;
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    // Negotiable
+    private boolean negotiable = true;
+
+    public boolean isNegotiable() {
+        return negotiable;
+    }
+
+    public void setNegotiable(boolean negotiable) {
+        this.negotiable = negotiable;
+    }
+
+    // Owner
+    private User owner;
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    // Status
+    private int status;
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
+
+    // Interested user
+    private Vector<User> interestedUsers;
+
+    public Vector<User> getInterestedUsers() {
+        return interestedUsers;
+    }
+
+    public void setInterestedUsers(Vector<User> interestedUsers) {
+        this.interestedUsers = interestedUsers;
+    }
+
+    public void addInterestedUser(User user) {
+        interestedUsers.add(user);
+    }
+
+    // instance constructor
+    {
+        interestedUsers = new Vector<User>();
+    }
+
+    // Constructor
+    public Book(String name, String author, int quality, int price, User owner, int status) {
+        this.name =  name;
+        this.author = author;
+        this.quality = quality;
+        this.price = price;
+        this.owner = owner;
+        this.status = status;
+    }
+
 }
--- BookSC/app/src/main/java/Model/Chat.java
+++ BookSC/app/src/main/java/Model/Chat.java
@@ -1,7 +1,24 @@
 package Model;
 
+import java.sql.Timestamp;
+
 /**
  * Created by Ritvik on 11/14/15.
  */
 public class Chat {
+
+    public final String message;
+    public final User fromUser;
+    public final User toUser;
+
+    // the time stamp
+    public final long timeStamp = System.currentTimeMillis();
+
+    // Constructor
+    public Chat(String message, User from, User to) {
+        this.message = message;
+        this.fromUser = from;
+        this.toUser = to;
+    }
+
 }
